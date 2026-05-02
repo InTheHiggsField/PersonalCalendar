df67ae879f788c5e96e241ea961de0d7fdba40d3
InTheHiggsField <inthehiggsfield@gmail.com>
2026-05-02 02:16:56 -0500

    Added a function that removes outdated events.

--- User_Input_Func.py
+++ User_Input_Func.py
@@ -36,7 +36,7 @@ def store_event_data(entries):
         json.dump(event_data, file, indent=4, sort_keys=True)
 
 def detailed_calendar():
-
+    list_fix()
     top = tk.Toplevel(root)
     top.focus_force()
     cal = Calendar(top, tooltipbackground = 'black', tooltipforeground = 'white', tooltipalpha = 0.9, tooltipdelay = 200)
@@ -53,13 +53,54 @@ def detailed_calendar():
             month = int(split_date[0])
             day = int(split_date[1])
             year = int(split_date[2])
-            pass_date = datetime.date(year=year, month=month, day=day)
+            pass_date = datetime.datetime(year=year, month=month, day=day)
             cal.calevent_create(pass_date, text =f"Name: {details['Event Name']}, Details: {details['Event Details']} ", tags = 'reminder')
             tk.Label(top, text=f"Date: {details["Insert Date in MM/DD/YYYY format"]} {details['Event Details']}").pack(padx=10, pady=10)
             top.update_idletasks()
     cal.tag_config('reminder', background='red', foreground='yellow')
     cal.pack(padx=10, pady=10)
     ttk.Label(top, text=f"You have {len(event_data)} events soon").pack(padx=10, pady=10)
+
+#This will be a function that organizes the data within the Json file.
+
+def list_fix():
+
+    with open('event_data.json', 'r') as file:
+        event_data = json.load(file)
+    for events, details in event_data.items():
+        #Parse the date into something we can apply the date data type to
+        if details == "delete":
+            continue
+        parse_date = details["Insert Date in MM/DD/YYYY format"].split('/')
+        date = datetime.datetime(int(parse_date[2]), int(parse_date[0]), int(parse_date[1]))
+
+        if date < datetime.datetime.today():
+            event_data[events] = "delete" #We will write a loop that deletes these
+    l = 1
+    length = len(event_data)
+    while l < length:
+        try:
+            if event_data[f"Event #{l}"] == "delete":
+                del event_data[f"Event #{l}"]
+        except Exception as e:
+            print(e)
+            pass
+        l += 1
+
+    while l < length: #This is technically a repeat of lines 82 - 89. We can definitely make this more efficient.
+        try:
+            if event_data[f"Event #{l}"] == "delete":
+                del event_data[f"Event #{l}"]
+        except Exception as e:
+            print(e)
+            pass
+        l += 1
+
+    with open('event_data.json', 'w') as file:
+        json.dump(event_data, file, indent=4, sort_keys=True)
+
+
+
 
 root = tk.Tk()
 ttk.Button(root, text="Add Event", command=add_event).pack(padx = 10, pady = 10)
